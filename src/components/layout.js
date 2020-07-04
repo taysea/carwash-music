901c6101d1dceddfd81be17c41bb911cfcd81c5e
Taylor Seamans <taylor.seamans@yahoo.com>
2020-07-04 10:06:54 -0700

    Photo only landing page

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import { Box, Grommet, Main, ResponsiveContext, Stack, Video } from 'grommet'
+import { Box, Grommet, Main, ResponsiveContext } from 'grommet'
 import Div100vh from 'react-div-100vh'
 import Navigation from './navigation'
 import { Footer } from './Footer'
 import { customTheme } from '../theme'
-import LandingVideo from '../assets/postweb-small.mp4'
 import LandingPhoto from '../assets/punch-32.jpg'
 import GlobalFonts from '../fonts/fonts'
 import './layout.css'
@@ -17,36 +16,17 @@ const Template = ({ children, height, isLanding }) => (
       <GlobalFonts />
       <ResponsiveContext.Consumer>
         {size => (
-          <Stack guidingChild={isLanding && size !== 'small' && 'last'} fill>
-            {isLanding && size !== 'small' && (
-              <Div100vh>
-                <Box fill>
-                  <Video
-                    controls={false}
-                    fit="cover"
-                    autoPlay
-                    loop
-                    muted
-                    playsInline
-                  >
-                    <source key="video" src={LandingVideo} type="video/mp4" />
-                  </Video>
-                </Box>
-              </Div100vh>
-            )}
-            <Div100vh style={height && { minHeight: '100rvh' }}>
-              <Box
-                background={
-                  isLanding &&
-                  size === 'small' && {
-                    image: `url(${LandingPhoto})`,
-                    position: 'left',
-                  }
+          <Div100vh style={height && { minHeight: '100rvh' }}>
+            <Box
+              background={
+                isLanding && {
+                  image: `url(${LandingPhoto})`,
+                  position: size !== 'small' ? 'bottom' : 'left',
                 }
-                fill={!height}
-                width={{ max: 'xxlarge' }}
-                margin="auto"
-              >
+              }
+              fill
+            >
+              <Box width={{ max: 'xxlarge' }} margin="auto" fill>
                 <Navigation />
                 <Main
                   flex
@@ -57,8 +37,8 @@ const Template = ({ children, height, isLanding }) => (
                 </Main>
                 <Footer isLanding={isLanding} />
               </Box>
-            </Div100vh>
-          </Stack>
+            </Box>
+          </Div100vh>
         )}
       </ResponsiveContext.Consumer>
     </ContextProvider>
