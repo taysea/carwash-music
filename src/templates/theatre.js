725896ef027ed944cff2c85e20c7349004694cc0
Taylor Seamans <taylor.seamans@yahoo.com>
2021-01-11 17:52:28 -0800

    Video grid layout

--- src/templates/theatre.js
+++ src/templates/theatre.js
@@ -1,11 +1,29 @@
-import React from 'react'
-import { graphql } from 'gatsby'
-import { Box, ResponsiveContext } from 'grommet'
+import React, { useEffect, useRef, useState } from 'react'
+import { Box, Grid, ResponsiveContext, Stack } from 'grommet'
 import Helmet from 'react-helmet'
 import Layout from '../components/layout'
 import favicon from '../images/favicon.ico'
 
 export default ({ location, data }) => {
+  const ref = useRef()
+  const [height, setHeight] = useState('400px')
+
+  useEffect(() => {
+    const updateHeight = () => {
+      if (ref && ref.current) {
+        const dimensions = ref.current && ref.current.getBoundingClientRect()
+        setHeight(`${dimensions.width * (9 / 16)}px`)
+      }
+    }
+    updateHeight()
+    window.addEventListener('resize', updateHeight)
+
+    return () => {
+      window.removeEventListener('resize', updateHeight)
+    }
+  }, [ref, ref.current])
+  // console.log(ref.current.getBoundingClientRect())
+
   return (
     <Layout location={location} height>
       <Helmet title="Theatre">
@@ -13,25 +31,26 @@ export default ({ location, data }) => {
       </Helmet>
       <ResponsiveContext.Consumer>
         {size => (
-          <Box
-            align="center"
+          <Grid
+            columns={size !== 'small' ? ['auto', 'auto'] : '100%'}
+            rows="auto"
+            gap="small"
             justify="center"
-            gap={size !== 'small' ? 'medium' : 'xlarge'}
-            margin={{ top: 'large' }}
+            fill
           >
             {data.allContentfulTheatre.edges.map(({ node }) => (
-              <Box width="large" height={size !== 'small' ? '450px' : '250px'}>
+              <Box ref={ref} height={height} width="100%">
                 <iframe
                   width="100%"
                   height="100%"
                   src={node.url}
                   frameborder="0"
                   allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                  allowfullscreen
+                  allowFullScreen
                 ></iframe>
               </Box>
             ))}
-          </Box>
+          </Grid>
         )}
       </ResponsiveContext.Consumer>
     </Layout>
